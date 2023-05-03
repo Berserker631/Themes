048a0197f0f9ff1a4707ccc5263c41fff9ce66bf
Ner aguilar <ner.aguilar06@gmail.com>
2023-05-03 16:59:43 -0400

    Temas nuevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,27 +8,36 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 export class AppComponent {
   title = 'Themes';
   showMenu = false;
-
+  menu = false;
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
-
-  test() {
-    let grid = document.querySelector('.gridBlock');
-
-  }
-  @ViewChild('grid', { static: false }) popo!: ElementRef;
-  constructor() {
-
+  dropDownMenu() {
+    this.menu = !this.menu;
   }
 
-  ngAfterViewInit() {
-    console.log();
+  storedTheme: string | null = localStorage.getItem('themeColor');
+  constructor() { }
+  ngOnInit() { }
 
-    let x = this.popo.nativeElement;
-    x.querySelector('.borderSquareButton').style.backgroundColor = "red";
+  setTheme(theme) {
+    localStorage.setItem('themeColor', theme);
+    this.storedTheme = localStorage.getItem('themeColor');
   }
+  // test() {
+  // }
+  // @ViewChild('grid', { static: false }) popo!: ElementRef;
+  // constructor() {
+
+  // }
+
+  // ngAfterViewInit() {
+  //   console.log();
+
+  //   let x = this.popo.nativeElement;
+  //   x.querySelector('.sqrSolidDark').style.backgroundColor = 'red';
+  // }
   // styleExp = 'red';
 
 
